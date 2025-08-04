113792c5ac6ce5b27cfc669822f03f87db5d936a
ksandeira <ostapksenia368@gmail.com>
2025-08-04 23:16:23 +0300

    fixed commit

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,12 @@
-import java.time.LocalDate;
-
 public class Main {
     //task 1
     public static void calculationLeapYear(int year) {
         boolean calculationLeapYear = (year % 400 == 0) || (year % 100 != 0 && year % 4 == 0);
         System.out.println(year + " год — " + (calculationLeapYear ? "является високосным годом" : "является невисокосным годом"));
     }
+
     //task 2
-    public static void installAppVersion(int osType, int deviceYear) {
-        int currentYear = LocalDate.now().getYear();
+    public static void installAppVersion(int osType, int clientDeviceYear) {
         String osName;
         if (osType == 0) {
             osName = "iOS";
@@ -18,9 +16,11 @@ public class Main {
             System.out.println("Неизвестный тип ОС");
             return;
         }
-        String version = (deviceYear < currentYear) ? "облегченную версию " : "";
+
+        String version = (clientDeviceYear < 2015) ? "облегченную версию " : "";
         System.out.println("Установите " + version + "приложения для " + osName + " по ссылке");
     }
+
     //task 3
     public static int calculateDeliveryDays(int distance) {
         if (distance <= 0) {
@@ -35,12 +35,14 @@ public class Main {
             return -1;
         }
     }
+
     public static void main(String[] args) {
         System.out.println("Task 1");
         calculationLeapYear(1754);
 
         System.out.println("Task 2");
-        installAppVersion(0, 2025);
+        int clientDeviceYear = 2015;
+        installAppVersion(0, clientDeviceYear);
 
         System.out.println("Task 3");
         int deliveryDistance = 95;
