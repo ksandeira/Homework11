330c787132711e8a95f46e364861b8899ac1ecc4
ksandeira <ostapksenia368@gmail.com>
2025-08-04 21:53:09 +0300

    finished commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,54 @@
-public class Main {
-    public static void main(String[] args) {
+import java.time.LocalDate;
 
+public class Main {
+    //task 1
+    public static void calculationLeapYear(int year) {
+        boolean calculationLeapYear = (year % 400 == 0) || (year % 100 != 0 && year % 4 == 0);
+        System.out.println(year + " год — " + (calculationLeapYear ? "является високосным годом" : "является невисокосным годом"));
+    }
+    //task 2
+    public static void installAppVersion(int osType, int deviceYear) {
+        int currentYear = LocalDate.now().getYear();
+        String osName;
+        if (osType == 0) {
+            osName = "iOS";
+        } else if (osType == 1) {
+            osName = "Android";
+        } else {
+            System.out.println("Неизвестный тип ОС");
+            return;
+        }
+        String version = (deviceYear < currentYear) ? "облегченную версию " : "";
+        System.out.println("Установите " + version + "приложения для " + osName + " по ссылке");
+    }
+    //task 3
+    public static int calculateDeliveryDays(int distance) {
+        if (distance <= 0) {
+            return -1;
+        } else if (distance <= 20) {
+            return 1;
+        } else if (distance <= 60) {
+            return 2;
+        } else if (distance <= 100) {
+            return 3;
+        } else {
+            return -1;
         }
     }
+    public static void main(String[] args) {
+        System.out.println("Task 1");
+        calculationLeapYear(1754);
+
+        System.out.println("Task 2");
+        installAppVersion(0, 2025);
+
+        System.out.println("Task 3");
+        int deliveryDistance = 95;
+        int deliveryTime = calculateDeliveryDays(deliveryDistance);
+        if (deliveryTime == -1) {
+            System.out.println("Доставка по адресу невозможна");
+        } else {
+            System.out.println("Потребуется дней: " + deliveryTime);
+        }
+    }
+}
